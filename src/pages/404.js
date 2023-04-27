c6f1de3c2d937729a9fdeaf058e930ef64c71616
Maciej Rybak <maciek.rybak95@gmail.com>
2023-04-28 00:31:51 +0200

    add custom pages

---
+++ src/pages/404.js
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+export default function NotFound() {
+  return (
+    <div>
+      <h2>404</h2>
+      <p>Sorry, that page doesn't exist</p>
+    </div>
+  );
+}
---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <p> About page</p>
+      <p> Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum </p>
+      <p> Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum </p>
+      <p> Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum </p>
+    </div>
+  );
+}
