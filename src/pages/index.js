d88fa2ec808c6b1c5d2afdf0320dfbb07f14b8c4
Maciej Rybak <maciek.rybak95@gmail.com>
2023-04-30 20:00:17 +0200

    add global css v2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,19 @@
+import { Link } from "gatsby";
 import * as React from "react";
 import Layout from "../components/Layout";
+import { header, btn } from "../styles/home.module.css";
 
 export default function Home() {
   return (
     <Layout>
-      <section>
+      <section className={header}>
         <div>
           <h2>Design</h2>
           <h3>Develop & Deploy</h3>
           <p>UX designer & web developer based in Manchester</p>
+          <Link className={btn} to="/projects">
+            My portfolio Projects
+          </Link>
         </div>
       </section>
     </Layout>
