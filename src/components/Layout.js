7142c5b2e5836160200f97ac01830bac849d0113
Maciej Rybak <maciek.rybak95@gmail.com>
2023-04-30 19:35:31 +0200

    add global css

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Navbar from "./Navbar";
+import "../styles/global.css";
+
 const Layout = ({ children }) => {
   return (
     <div className="layout">
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <nav>
-      <h1>Wev Warrior</h1>
+      <h1>Web Warrior</h1>
       <div className="links">
         <Link to="/">Home </Link>
         <Link to="/about">About </Link>
