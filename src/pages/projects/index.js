a118114898921b43ae4f519513a113014b36e84d
Maciej Rybak <maciek.rybak95@gmail.com>
2023-05-14 16:19:08 +0200

    add graphql to markdownpages, css update

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,14 +1,43 @@
 import * as React from "react";
 import Layout from "../../components/Layout";
-import { portfolio } from "../../styles/projects.module.css";
+import { portfolio, projectss } from "../../styles/projects.module.css";
+import { Link, graphql } from "gatsby";
 
-export default function Projects() {
+export default function Projects({ data }) {
+  console.log(data);
+  const projects = data.allMarkdownRemark.nodes;
+
   return (
     <Layout>
       <div className={portfolio}>
         <h2>Portfolio</h2>
         <h3>Projects & Websites I've Created</h3>
+        <div className={projectss}>
+          {projects.map((project) => (
+            <Link to={"/projects" + project.frontmatter.slug} key={project.id}>
+              <div>
+                <h3>{project.frontmatter.title}</h3>
+                <p>{project.frontmatter.stack}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </Layout>
   );
 }
+
+export const query = graphql`
+  query MyQuery {
+    allMarkdownRemark {
+      nodes {
+        frontmatter {
+          slug
+          stack
+          title
+        }
+        id
+      }
+    }
+  }
+`;
